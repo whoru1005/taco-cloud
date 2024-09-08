df7f7d99b8a4b284612ac11180bb78c8d4357f72
whorum <wjdtpwls1005@gmail.com>
2024-09-08 22:12:21 +0900

    [FIX] designTacoController 버그 수정

--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -52,18 +52,37 @@ public class DesignTacoController
     }
 
     @PostMapping
-    public String processDesign(@Valid Taco design, Errors errors){
+    public String processDesign(@Valid Taco design, Errors errors, Model model){
 
-        if(errors.hasErrors()) {
-            return "design";
+        if (errors.hasErrors()) {
+            // 유효성 오류가 있을 때 재료 목록을 다시 추가
+            List<Ingredient> ingredients = Arrays.asList(
+                    new Ingredient("FlTO", "Flour Tortilla", Ingredient.Type.WRAP),
+                    new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP),
+                    new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN),
+                    new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
+                    new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES),
+                    new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES),
+                    new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE),
+                    new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE),
+                    new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
+                    new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE)
+            );
+
+            Ingredient.Type[] types = Ingredient.Type.values();
+            for(Type type: types) {
+                model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
+            }
+
+            return "design";  // 유효성 오류가 있으면 다시 디자인 페이지로 이동
         }
-
 
         //타코 디자인 저장
         log.info("Processing design: " + design);
 
-        return "redirect:/orders/current";
+        return "redirect:/orders/current";  // 유효성 검사를 통과하면 주문 페이지로 리다이렉트
     }
+
 
     private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type)
     {
